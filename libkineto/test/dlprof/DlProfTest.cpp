e0949930e705a85faff314e406d74764fc07e2cb
yuanjian <goodjobwubai@gmail.com>
2024-01-12 22:25:40 +0800

    add first test case: call_id test

--- libkineto/test/dlprof/DlProfTest.cpp
+++ libkineto/test/dlprof/DlProfTest.cpp
@@ -3,6 +3,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <vector>
+#include <unordered_map>
 
 #include "hc/profile.h"
 #include "hc/activity.h"
@@ -20,7 +21,6 @@ using json = nlohmann::json;
 class DlProfTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    std::cout << "dlprof setup" << std::endl;
     platform = Platform::GetInstance();
     ASSERT_EQ(platform->GetDevice(&device, 0), kHcSuccess);
     ASSERT_EQ(device->GetID(), 0);
@@ -56,11 +56,12 @@ class DlProfTest : public ::testing::Test {
 };
 
 TEST_F(DlProfTest, callid) {
-    std::cout << "hello dlprof" << std::endl;
     int* hostArray = nullptr;
     int* deviceArray = nullptr;
-    int dataCount = 11;
-    std::vector<uint32_t> callid;
+    int dataCount = 1000;
+    std::unordered_map<int, int> info_pre; // size : callid
+    std::unordered_map<int, std::pair<int, int>> info_post; // size: callid
+    std::set<int> traceid_set;
     hostArray = createAscendHostMemory(dataCount);
     cudaMalloc((void**)&deviceArray, dataCount * sizeof(int));
 
@@ -69,7 +70,7 @@ TEST_F(DlProfTest, callid) {
     ASSERT_EQ(profiler_->BeginPass(0), kHcSuccess);
     for (int i = 1; i < dataCount; i++) {
         ProfileFunctionCallBegin();
-        callid.push_back(ProfileFunctionCallId());
+        info_pre[i * sizeof(int)] = ProfileFunctionCallId();
         cudaMemcpy(deviceArray, hostArray, i * sizeof(int), cudaMemcpyHostToDevice);
         ProfileFunctionCallEnd();
     }
@@ -80,24 +81,48 @@ TEST_F(DlProfTest, callid) {
     std::shared_ptr<ProfileReport> report;
     HCCreateProfileParser(&parser, profiler_.get());
     ASSERT_EQ(parser->Parse(report, 0), kHcSuccess);
-//    report->DumpMetrics(std::cout);
 
     std::stringstream ss;
     report->ExportJson(ss);
-    std::string str = ss.str();
-    json data = json::parse(str);
-    std::cout << data["metrics"] << std::endl;
+    json data = json::parse(ss);
 
     for (auto& element : data["metrics"]) {
-        std::cout << element << '\n';
+        int trace_id = element["trace_id"].template get<int>();
+        traceid_set.insert(trace_id);
     }
-    std::cout << std::endl;
+
     for (auto& element : data["command_repo"]) {
-        std::cout << element << '\n';
+        int trace_id = element["trace_id"].template get<int>();
+        auto it = traceid_set.find(trace_id);
+        if (it != traceid_set.end()) {
+            if (element.contains("args")) {
+                std::array<int, 1> size =  element["args"]["size"].template get<std::array<int,1>>();
+                info_post[trace_id] = std::make_pair(size[0], -1);
+            }
+        }
     }
     std::cout << std::endl;
     for (auto& element : data["submits"]) {
-        std::cout << element << '\n';
+        int trace_id = element["trace_id"].template get<int>();
+        auto it = traceid_set.find(trace_id);
+        if (it != traceid_set.end()) {
+            int call_id = element["call_id"].template get<int>();
+            info_post[trace_id] = std::make_pair(info_post[trace_id].first, call_id);
+        }
+    }
+
+    ASSERT_EQ(info_pre.size(), info_post.size());
+
+    // traverse the info_post, compare it with info_pre
+    for (const auto& pair : info_post) {
+        int size = pair.second.first;
+        int call_id = pair.second.second;
+        auto it = info_pre.find(size);
+        if (it != info_pre.end()) {
+            ASSERT_EQ(call_id, it->second);
+        } else {
+            FAIL() << "can't find corresponding call_id!" << std::endl;
+        }
     }
 
     cudaFree(deviceArray);
